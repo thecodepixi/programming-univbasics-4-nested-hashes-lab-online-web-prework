883021df4f629c2baf4804aca2d68914bda53a37
Emily Harber <emily.a.harber@gmail.com>
2019-08-15 01:00:38 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:alan_kay][:known_for] 
+    
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:dennis_ritchie][:languages][0]  
+     
 end
 
 def adding_matz
@@ -80,6 +87,9 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP","C"]}
+
+programmer_hash
     
 end
 
@@ -102,6 +112,10 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
+  programmer_hash
+     
      
 end
 
@@ -121,5 +135,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  
+  programmer_hash
 
 end
